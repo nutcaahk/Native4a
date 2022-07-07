c341922a424f588e03024220c1fa36d386327c71
Taki Kwan <nativeaaaa@gmail.com>
2022-07-07 10:08:08 +0800

    setup tailwindcss

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sharp",
     "gatsby-plugin-image",
+    "gatsby-plugin-postcss",
     {
       resolve: "gatsby-source-contentful",
       options: {
